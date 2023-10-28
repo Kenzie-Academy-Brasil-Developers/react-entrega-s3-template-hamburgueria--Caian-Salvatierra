bae8038196af6cae69b7ae75b658dd51220a7560
Caian-Salvatierra <caian_salvatierra97@outlook.com>
2023-10-28 00:43:35 -0300

    Atualização de estilização do projeto

--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -3,7 +3,7 @@ import style from "./style.module.scss";
 export const ProductCard = ({ product, addCart }) => {
     return(
         <div className="container">
-        <li className={style.cardList}>
+        <li onClick={(product) => `${style.cardList}`}>
             <img src={product.img} alt={product.name} />
             <div className={style.boxCard}>
                 <h3 className="title third">{product.name}</h3>
